16c6db69a881afc931b8a1ea34f85cf21d272c9f
AlexeyKurlukov <kuralex92@mail.ru>
2024-01-02 16:13:57 +0700

    Fixed bugs in ArraysManipulation directory

--- ArraysManipulation/script.js
+++ ArraysManipulation/script.js
@@ -1,28 +1,40 @@
-const array = [-5, 5, 5, 5, 5, 2, 15, 4, 11, 15];
+(function () {
+    function getDescendingSortedArray(numbersArray) {
+        return numbersArray.slice()
+            .sort((number1, number2) => number2 - number1);
+    }
 
-const sortArray = (array) => {
-    return array.slice().sort((element1, element2) => element2 - element1);
-};
+    function getFirstNumbers(numbersArray, numbersCount) {
+        return numbersArray.slice(0, numbersCount);
+    }
 
-const getSubArray = (array) => {
-    const firstFive = array.slice(0, 5);
-    const lastFive = array.slice(-5);
-    return {firstFive, lastFive};
-};
+    function getLastNumbers(numbersArray, numbersCount) {
+        return numbersArray.slice(-numbersCount);
+    }
 
-const getEvenNumbersSum = (array) => {
-    return array.filter(element => element % 2 === 0).reduce((accumulated, current) => accumulated + current, 0);
-};
+    function getEvenNumbersSum(numbersArray) {
+        return numbersArray.filter(number => number % 2 === 0)
+            .reduce((sum, number) => sum + number, 0);
+    }
 
-const hundredElementsArray = Array.from({length: 100}, (_, i) => i + 1);
+    function getEvenNumbersSquares(numbersArray) {
+        return numbersArray.filter(number => number % 2 === 0)
+            .map(number => Math.pow(number, 2));
+    }
 
-const getEvenNumbersSquares = (hundredElementsArray) => {
-    return hundredElementsArray.filter(element => element % 2 === 0).map(element => Math.pow(element, 2));
-};
+    const numbersArray1 = [-5, 5, 5, 5, 5, 2, 15, 4, 11, 15];
 
-console.log("Исходный массив:", array);
-console.log("Сортировка по убыванию:", sortArray(array));
-console.log("Подмассив из первых 5 элементов:", getSubArray(array).firstFive);
-console.log("Подмассив из последних 5 элементов:", getSubArray(array).lastFive);
-console.log("Сумма чётных чисел:", getEvenNumbersSum(array));
-console.log("Квадраты чётных чисел от 1 до 100:", getEvenNumbersSquares(hundredElementsArray));
+    console.log("Исходный массив:", numbersArray1);
+    console.log("Сортировка по убыванию:", getDescendingSortedArray(numbersArray1));
+    console.log("Подмассив из первых 5 элементов:", getFirstNumbers(numbersArray1, 5));
+    console.log("Подмассив из последних 5 элементов:", getLastNumbers(numbersArray1, 5));
+    console.log("Сумма чётных чисел:", getEvenNumbersSum(numbersArray1));
+
+    const numbersArray2 = [];
+
+    for (let i = 1; i <= 100; i++) {
+        numbersArray2.push(i);
+    }
+
+    console.log("Квадраты чётных чисел от 1 до 100:", getEvenNumbersSquares(numbersArray2));
+})();
